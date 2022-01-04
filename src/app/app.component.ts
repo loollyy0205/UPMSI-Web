ee959c1458393f139e5fd06603cdee77ed520012
UP-MSI <website@msi.upd.edu.ph>
2022-01-04 20:50:40 +0800

    -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,13 +12,13 @@ export class AppComponent implements OnInit {
   location: Location;
 
   ngOnInit() {
-  /*
+  
     if (environment.production) {
       if (location.protocol === 'http:') {
         window.location.href = location.href.replace('http', 'https');
       }
     }
-**/
+
    
   }
 
